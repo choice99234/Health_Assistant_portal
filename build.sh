7d3683324f8ba2c91ad4cf843a80a2fa6109ab9f
Choice Kunthani <kunthanichoice@gmail.com>
2024-12-15 10:24:02 +0200

    initial commit

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Build the Docker image
-docker build -t my-vercel-app .
-
-# Push to Docker if needed, or just run locally in Vercel's environment
-# DockerHub login can be added if you need to push the image to DockerHub or other registries
